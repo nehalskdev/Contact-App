77c32af1e3462fb37b76a7dc1d3b1bfd23e4a76b
nehalskdev <nikesk1999@gmail.com>
2024-10-07 00:51:13 +0530

    added close modal functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const addBtn = document.querySelector('.add-btn');
-const openModal = document.querySelector('.modal');
+const addBtn = document.querySelector(".add-btn");
+const openModal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close");
 
-    addBtn.addEventListener('click', () => {
-        console.log('Hit');
-        openModal.style.display = 'block';
-    
+addBtn.addEventListener("click", () => {
+  console.log("open");
+  openModal.style.display = "block";
 });
-    
 
+closeModal.addEventListener("click", () => {
+  console.log("closed");
+  openModal.style.display = "none";
+});
