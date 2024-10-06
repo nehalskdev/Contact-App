6ad573789b3f263ad2f94cf29792741281e1ee69
nehalskdev <nikesk1999@gmail.com>
2024-10-07 00:44:08 +0530

    added functionality for opening modal

---
+++ app.js
@@ -0,0 +1,11 @@
+const addBtn = document.querySelector('.add-btn');
+const openModal = document.querySelector('.modal');
+const closeModal = document.querySelector(".close");
+
+    addBtn.addEventListener('click', () => {
+        console.log('Hit');
+        openModal.style.display = 'block';
+    
+});
+    
+
