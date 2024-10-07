a5c36ac0ab0ca34d516141c71cc3fc004b4b9742
nehalskdev <nikesk1999@gmail.com>
2024-10-07 14:20:57 +0530

    added comments in js file

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const addBtn = document.querySelector(".add-btn");
 const openModal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close");
 
+// opening the modal
 addBtn.addEventListener("click", () => {
   console.log("open");
   openModal.style.display = "block";
 });
 
+// closing the modal
 closeModal.addEventListener("click", () => {
   console.log("closed");
   openModal.style.display = "none";
